978e9ffb988cb590140ab97a92cb1ae9ee7efde0
Davide Aversa <thek3nger@gmail.com>
2014-02-11 16:34:56 +0100

    New BasicLogger

---
+++ Assets/RoR/Logging/BasicLogger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RoomOfRequirement.Logger {
+
+    public class BasicLogger {
+
+        private string tag;
+        
+        public BasicLogger(string tag) {
+            this.tag = tag;
+        }
+
+        public void Log(string message) {
+            BasicLogger.Log(tag, message);
+        }
+
+        public void LogError(string message) {
+            BasicLogger.LogError(tag, message);
+        }
+
+        public void LogWarning(string message) {
+            BasicLogger.LogWarning(tag, message);
+        }
+
+        public static void Log(string tag, string message) {
+            Debug.Log(string.Format("[{0}]: {1}", tag, message));
+        }
+
+        public static void LogError(string tag, string message) {
+            Debug.LogError(string.Format("[{0}]: {1}", tag, message));
+        }
+
+        public static void LogWarning(string tag, string message) {
+            Debug.LogWarning(string.Format("[{0}]: {1}", tag, message));
+        }
+    }
+
+}
